ef068c898fdd818d690d48dcda10fbcb8168b13e
Vinko Vacek <vinko.vacek@gmail.com>
2020-03-16 15:02:42 +0100

    added root query for testing

---
+++ wp-graphql-wpml.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Plugin Name: WP GRAPHQL WPML
+Version: 0.1
+Description: Adds language meta to posts as result of GraphQL query.
+Author: Vinko Vacek
+*/
+
+use GraphQLRelay\Relay;
+
+add_action( 'graphql_register_types', 'wp_graphql_wpml_init', 10, 0);
+
+function wp_graphql_wpml_init() {
+
+
+
+  register_graphql_field('RootQuery', 'defaultLanguage', [
+    'type' => 'Language',
+    'description' => __('Default site language', 'wp-graphql-wpml'),
+    'resolve' => function ($source, $args, $context, $info) {
+        $fields = $info->getFieldSelection();
+        $language = 'hrv';
+
+        return $language;
+    },
+  ]);
+
+
+}
+
