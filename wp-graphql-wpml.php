4c2a7acb701d784e5a60383dfafd9d497b317143
Vinko Vacek <vinko.vacek@gmail.com>
2020-03-16 18:54:05 +0100

    added root query for testing

--- wp-graphql-wpml.php
+++ wp-graphql-wpml.php
@@ -131,6 +131,7 @@ function map_language_to_query_args(
   array $query_args,
   array $where_args
 ) {
+  $query_args['suppress_filters'] = true;
   if (!isset($where_args['language'])) {
       return $query_args;
   }
