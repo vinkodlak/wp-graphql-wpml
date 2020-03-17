7d3033a480e7346bc4824c0c266f18e9530ff770
Vinko Vacek <vinko.vacek@gmail.com>
2020-03-17 11:02:41 +0100

    added TermObject

--- src/TermObject.php
+++ src/TermObject.php
@@ -111,7 +111,7 @@ class TermObject
                       'code' => 'hr',
                       'slug' => 'type'.$type,
                       'name' => $term->term_id,
-                      'locale' => $term_language_code,
+                      'locale' => print_r($term_language_code, true),
                     ];
                     return null;
                 // }
